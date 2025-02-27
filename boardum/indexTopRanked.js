8a87f01eaaeff8c145a34f56f33ea69988ef2adc
Jordyn Liegl <jordyn-liegl@Jordyns-PC>
2025-02-27 13:54:52 -0500

    Pages 121-135

--- boardum/indexTopRanked.js
+++ boardum/indexTopRanked.js
@@ -118,8 +118,8 @@ async function saveToJSON(games, pageNumber) {
   try {
     const { browser, page } = await loginToBGG();
 
-    const startPage = 120; // Continue from page 11
-    const endPage = 120;  // Adjust as needed
+    const startPage = 121; // Continue from page 11
+    const endPage = 135;  // Adjust as needed
 
     for (let p = startPage; p <= endPage; p++) {
       console.log(`\n--- Scraping Page ${p} ---`);
