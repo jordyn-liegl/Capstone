8f9dfdc65644d0768ebff8cefed8e5f5faeb0c8a
Selena Zheng <zheng.2012@osu.edu>
2025-04-01 13:36:22 -0400

    fix code formatting, prettier

--- boardum/src/app/page.tsx
+++ boardum/src/app/page.tsx
@@ -358,55 +358,54 @@ export default function Home() {
             </div>
           </motion.div>
         )}
-
-{selectedGame && (
-  <div className={styles.overlay} onClick={() => setSelectedGame(null)}>
-    <motion.div
-      className={styles.modal}
-      initial={{ scale: 0.8, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ duration: 0.3 }}
-      onClick={(e) => e.stopPropagation()} // Prevent closing modal on inner click
-    >
-      <div className={styles.contentContainer}>
-        <div className={styles.topRow}>
-          <img 
-            src={selectedGame.image} 
-            alt={selectedGame.name} 
-            className={styles.gameImage} 
-          />
-          <div className={styles.textColumn}>
-            <h2 className={styles.gameTitle}>{selectedGame.name}</h2>
-
-            <div className={styles.tagGroup}>
-              <div className={styles.tagLabel}>Category:</div>
-              <div className={styles.tagList}>
-                {selectedGame.categories.map((cat, index) => (
-                  <span key={index} className={styles.tagButton}>{cat}</span>
-                ))}
+        
+        {selectedGame && (
+          <div className={styles.overlay} onClick={() => setSelectedGame(null)}>
+            <motion.div
+            className={styles.modal}
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            onClick={(e) => e.stopPropagation()} // Prevent closing modal on inner click
+            >
+              <div className={styles.contentContainer}>
+                <div className={styles.topRow}>
+                  <img 
+                  src={selectedGame.image} 
+                  alt={selectedGame.name} 
+                  className={styles.gameImage} 
+                  />
+                  <div className={styles.textColumn}>
+                  <h2 className={styles.gameTitle}>{selectedGame.name}</h2>
+                  <div className={styles.tagGroup}>
+                    <div className={styles.tagLabel}>Category:</div>
+                    <div className={styles.tagList}>
+                      {selectedGame.categories.map((cat, index) => (
+                        <span key={index} className={styles.tagButton}>{cat}</span>
+                      ))}
+                    </div>
+                  </div>
+                  
+                  <div className={styles.tagGroup}>
+                    <div className={styles.tagLabel}>Mechanism:</div>
+                    <div className={styles.tagList}>
+                      {selectedGame.mechanics.map((mech, index) => (
+                        <span key={index} className={styles.tagButton}>{mech}</span>
+                      ))}
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-
-            <div className={styles.tagGroup}>
-              <div className={styles.tagLabel}>Mechanism:</div>
-              <div className={styles.tagList}>
-                {selectedGame.mechanics.map((mech, index) => (
-                  <span key={index} className={styles.tagButton}>{mech}</span>
-                ))}
+              
+              <p 
+              className={styles.gameSummary} 
+              dangerouslySetInnerHTML={{ __html: selectedGame.description.replace(/&#10;&#10;/g, '<br /><br />') }} 
+              />
               </div>
-            </div>
+              <button className={styles.closeButton} onClick={() => setSelectedGame(null)}>x</button>
+            </motion.div>
           </div>
-        </div>
-
-        <p 
-          className={styles.gameSummary} 
-          dangerouslySetInnerHTML={{ __html: selectedGame.description.replace(/&#10;&#10;/g, '<br /><br />') }} 
-        />
-      </div>
-      <button className={styles.closeButton} onClick={() => setSelectedGame(null)}>x</button>
-    </motion.div>
-  </div>
-)}
+        )}
       </main>
     </div>
   );
